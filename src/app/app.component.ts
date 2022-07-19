2d02e7cff1a70d8fa55a59344bb5730383da1ae3
Logan Murray <lpmurray16@users.noreply.github.com>
2022-07-19 17:28:40 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'team-app';
+  newMemberName: string = '';
+  members: string[] = [];
+  errorMessage: string = '';
+
+  addMember() {
+    if (this.newMemberName.length > 0) {
+      this.members.push(this.newMemberName);
+      this.newMemberName = '';
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = 'Name cannot be empty';
+    }
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
 }
